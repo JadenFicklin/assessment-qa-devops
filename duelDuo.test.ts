cbe0acb77b562b69be512b9c14b6dc5b2e92bbb4
JadenFicklin <jadendficklin@gmail.com>
2022-01-28 11:10:18 -0700

    added automated test

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -1,20 +1,33 @@
+import { Builder, Capabilities, By } from "selenium-webdriver";
 
-import { Builder, Capabilities, By } from "selenium-webdriver"
+require("chromedriver");
 
-require('chromedriver')
-
-const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
+const driver = new Builder().withCapabilities(Capabilities.chrome()).build();
 
 beforeEach(async () => {
-    driver.get('http://localhost:3000/')
-})
+  driver.get("http://localhost:3000/");
+});
 
 afterAll(async () => {
-    driver.quit()
-})
+  driver.quit();
+});
 
-test('Title shows up when page loads', async () => {
-    const title = await driver.findElement(By.id('title'))
-    const displayed = await title.isDisplayed()
-    expect(displayed).toBe(true)
-})
+test("Title shows up when page loads", async () => {
+  const title = await driver.findElement(By.id("title"));
+  const displayed = await title.isDisplayed();
+  expect(displayed).toBe(true);
+});
+
+// Check that clicking the Draw button displays the div with id = “choices”
+test("Check that clicking the Draw button displays the div with id = choices", async () => {
+  //find draw button and then click it
+  await driver.findElement(By.id("draw")).click();
+  //wait 2 seconds
+  await driver.sleep(2000);
+  //find choices
+  const getChoices = await driver.findElement(By.id("choices"));
+  //choices displayed
+  const displayChoices = await getChoices.isDisplayed();
+  //epect the choices displayed to be true
+  expect(displayChoices).toBe(true);
+});
