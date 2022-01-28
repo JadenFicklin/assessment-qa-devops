335af1184780829e7f2c20e9371683b54eab2da3
JadenFicklin <jadendficklin@gmail.com>
2022-01-28 10:54:20 -0700

    added 2 unit tests in functions.test.ts

--- functions.test.ts
+++ functions.test.ts
@@ -1,5 +1,17 @@
-const {shuffleArray} = require('./utils')
+const { shuffleArray } = require("./utils");
 
-describe('shuffleArray should', () => {
-    // CODE HERE
-})
+describe("shuffleArray should", () => {
+  //write atleast 2 tests that describe callback for shuffleArray function
+
+  //check that shuffleArray returns an array
+  //input array because shuffleArray method requires array
+  let input = [];
+  test("check that shuffleArray returns an array", () => {
+    //expect that it will return shuffled array input to be true
+    expect(Array.isArray(shuffleArray(input))).toBe(true);
+  });
+  test("check that it returns an array of the same length as the argument sent in", () => {
+    //shuffled arrays input should be the same length as input length
+    expect(shuffleArray(input).length).toBe(input.length);
+  });
+});
